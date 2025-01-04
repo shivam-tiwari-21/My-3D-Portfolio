aa163f1a4db3678608a31299e66de2d289eaa91c
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-01-04 20:57:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port,'0.0.0.0' () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
