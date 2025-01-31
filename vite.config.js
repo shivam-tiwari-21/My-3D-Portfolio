c420b68e5257256dcbb594efd0139a7a1a19c4b5
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-01-31 13:02:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Adjust this if deploying to a subdirectory
+  base: './', // Adjust this if deploying to a subdirectory
   build: {
     outDir: 'build', // Change output folder to 'build'
     rollupOptions: {
