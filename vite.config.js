bc8c0938909c7373e9f49d7092c78e5e3c2043e1
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-01-31 13:39:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: './', // Adjust this if deploying to a subdirectory
   build: {
-    outDir: 'build', // Change output folder to 'build'
+    outDir: 'dist', // Change output folder to 'dist'
     rollupOptions: {
       output: {
         manualChunks: {
