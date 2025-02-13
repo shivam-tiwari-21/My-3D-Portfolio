ef27a4818a2dcda9612f7c12462a4d82bf906f04
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-02-13 15:57:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <div className="w-full h-full fixed inset-0 z-[-1] pointer-events-none">
+      <div className="relative z-0 bg-primary">
           {/* StarsCanvas rendered globally */}
           <StarsCanvas />
           
