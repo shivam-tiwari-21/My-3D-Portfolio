aa021093e31a0bf73abfe1d26dfdb8824329dfd1
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-02-13 09:28:24 +0530

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,27 +4,38 @@ import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
-  const ref =useRef();
-  const sphere= random.inSphere(new Float32Array(5000), {radius: 1.2})
+  const sphere = random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 });
+const pointsRef = useRef();
+const geometryRef = useRef();
 
-  useFrame((state, delta)=>{
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  })
-  return (
-    <group rotation={[0,0, Math.PI/4]}>
-      <Points ref={ref} positions={sphere} stride={3}
-       frustumCulled {...props} >
-        <PointMaterial
+useFrame((state, delta) => {
+  if (pointsRef.current) {
+    pointsRef.current.rotation.x -= delta / 10;
+    pointsRef.current.rotation.y -= delta / 15;
+  }
+});
+
+return (
+  <group ref={pointsRef} rotation={[0, 0, Math.PI / 4]}>
+    <points>
+      <bufferGeometry attach="geometry" ref={geometryRef}>
+        <bufferAttribute
+          attach="attributes-position"
+          array={sphere}
+          count={sphere.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <PointMaterial
         transparent
         color="#f272c8"
         size={0.002}
-        sizeAttenuation={true}
+        sizeAttenuation
         depthWrite={false}
-        />
-       </Points>
-    </group>
-  )
+      />
+    </points>
+  </group>
+);
 }
 const StarsCanvas = () => {
   return (
