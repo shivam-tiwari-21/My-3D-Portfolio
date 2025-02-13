c45cc148d077e3c5d999f65ea725c952867999f8
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-02-13 18:03:23 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/hero-bg.webp')",
       },
     },
   },
