8fddcbd5a5880013ee80da520d40527f577b216b
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-02-13 15:03:04 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full min-h-screen mx-auto py-16">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -39,7 +39,7 @@ const Hero = () => {
       </div>
 
       {/* Animated div component placed below modal */}
-      <div className="absolute w-full flex justify-center items-center z-10">
+      <div className="w-full flex justify-center items-center z-10 mt-10">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
