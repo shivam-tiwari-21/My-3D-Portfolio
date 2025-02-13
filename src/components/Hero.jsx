6a8a810b36a62fcdc0b542730b40e06788a820be
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-02-13 14:55:35 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -14,19 +14,21 @@ const Hero = () => {
         </div>
         <div>
           <motion.h1 
-            initial={{y:25, opacity:0}}
-            animate={{y:0, opacity:1}}
-            transition={{duration:2, ease:"easeInOut"}}
-            className={${styles.heroHeadText} text-white}>
+            initial={{ y: 25, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+            className={`${styles.heroHeadText} text-white text-center`}
+          >
             Hi, I'm <span className="text-[#915eff]">Shivam Tiwari</span>
           </motion.h1>
           <motion.p
-            style={{overflow:"hidden",whiteSpace:"nowrap"}}
-            initial={{width:0}}
-            animate={{width:"100%"}}
-            transition={{duration:4, ease:"easeInOut"}}
-            className={${styles.heroSubText} mt-2 text-white-100}>
-            I develop Full-Stack Web Applications, <br className="sm:block hidden" />
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+            className={`${styles.heroSubText} mt-2 text-white-100 text-center`}
+          >
+            I develop Full-Stack Web Applications
+            <br className="sm:block hidden" />
             primarily using the MERN Stack from Scratch
           </motion.p>
         </div>
@@ -34,8 +36,7 @@ const Hero = () => {
 
       <div className="section-container">
         {/* <ComputersCanvas /> */}
-</div>
-
+      </div>
 
       {/* Animated div component placed below modal */}
       <div className="absolute w-full flex justify-center items-center z-10">
